24cb64eebb851c52aa9b7477660e1e516dccac1d
Marina Gotovtseva <m.gotovtseva@labmedia.su>
2022-08-23 18:24:47 +0300

    Handling multiple cities response

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
               @click="getCurrentWeather('local')">Show local weather</button>
       <button class="btn"
               @click="getCoordsByCityName">Get coords by city name</button>
+      <button class="btn"
+              @click="handleMultipleCities">Handle multiple cities</button>
 
       <form @submit.prevent @keydown.enter="enterKeyTest">
         <SearchCity v-model="formData.searchCityQuery" title="Or enter a city" :error="formData.searchError" placeholder="City" />
@@ -29,6 +31,17 @@
                    class="btn"
                    value="Show city search weather"
                    @click="getCurrentWeather('searchCity')">
+        <ul class="multiple__cities" v-if="citiesData">
+          <!-- Use spans to display all info -->
+          <li v-for="(city, index) in citiesData" :key="index">
+            <span>{{ city.name }}</span>
+            <span>{{ city.country }}</span>
+            <!-- <img src="http://openweathermap.org/images/flags/`${city.country}.toLowerCase()`.png" alt="flag"> -->
+            <span>{{ city.state }}</span>
+            <span>{{ city.lat }}</span>
+            <span>{{ city.lon }}</span>
+            </li>
+        </ul>
       </form>
     </div>
     <div class="display" v-if="isDataLoaded">
@@ -66,11 +79,16 @@ export default {
         lat: null,
         lon: null,
       },
+      coords: {
+        lat: null,
+        lon: null,
+      },
       formData: {
         searchCityQuery: null,
         searchError: null,
       },
       isDataLoaded: false,
+      citiesData: null,
       currentConditions: {
         // city: '',
         // country: '',
@@ -106,26 +124,46 @@ export default {
         // console.log(position);
         this.currentPosition.lat = position.coords.latitude;
         this.currentPosition.lon = position.coords.longitude;
+        this.coords.lat = position.coords.latitude;
+        this.coords.lon = position.coords.longitude;
       } catch (err) {
         console.error(err.message);
       }
     },
 
+    // eslint-disable-next-line consistent-return
     async getCoordsByCityName() {
-      const response = await axios.get(API_GEOCODING, {
-        params: {
-          q: this.formData.searchCityQuery,
-          appid: API_KEY,
-          limit: 5,
-        },
-      });
-      console.log(response.data, response.data.length);
-      if (!response.data.length) {
-        return '';
+      try {
+        if (this.formData.searchCityQuery === null || '') {
+          throw new Error('Please provide a city name!');
+        }
+        const response = await axios.get(API_GEOCODING, {
+          params: {
+            q: this.formData.searchCityQuery,
+            appid: API_KEY,
+            limit: 5,
+          },
+        });
+        // console.log(response.data, response.data.length);
+        if (response.data.length > 0) {
+          this.coords.lat = response.data[0].lat;
+          this.coords.lon = response.data[0].lon;
+          this.citiesData = response.data;
+        } else {
+          throw new Error('Nothing arrived');
+        }
+        // console.log(response.data);
+        return response.data;
+      } catch (err) {
+        console.error(err.message);
+        // return err.message;
       }
-      return response.data[0];
       // If response.data has several cities, we ask the user which one and pass
       // that data into the getCurrentWeather('searchCity').
+    },
+
+    handleMultipleCities() {
+      return this.citiesData;
     },
 
     async getCurrentWeather(location) {
@@ -133,44 +171,36 @@ export default {
       let lon;
       if (location === 'local') {
         await this.getCurrentCoords();
-        ({ lat, lon } = this.currentPosition);
-        console.log(lat, lon);
       } else if (location === 'searchCity') {
-        if (this.formData.searchCityQuery === '') {
-          console.error('Please provide a city name!');
-          return;
-        }
-        const coordsResponse = await this.getCoordsByCityName();
-        console.log(coordsResponse);
-        if (!coordsResponse) {
-          console.error('Nothing was found');
-          this.formData.searchError = 'Nothing was found';
-        } else {
-          ({ lat, lon } = coordsResponse);
-          const response = await axios.get(API_CURRENT_CONDITIONS, {
-            params: {
-              lat,
-              lon,
-              appid: API_KEY,
-              units: 'metric',
-            },
-          });
-          // console.log(response.data);
-          this.isDataLoaded = true;
-          this.apiData = response.data || {};
-          this.currentConditions = {
-            city: response.data.name,
-            country: response.data.sys.country,
-            temp: response.data.main.temp,
-            feelsLike: response.data.main.feels_like,
-            humidity: response.data.main.humidity,
-            description: response.data.weather[0].main,
-            icon: response.data.weather[0].icon,
-            windDir: response.data.wind.deg,
-            windSpeed: response.data.wind.speed, // meter/sec or miles/hour
-          } || {};
-        }
+        await this.getCoordsByCityName();
       }
+      if (!this.coords.lat || !this.coords.lon) {
+        throw new Error('Empty coords');
+      } else {
+        ({ lat, lon } = this.coords);
+      }
+      const response = await axios.get(API_CURRENT_CONDITIONS, {
+        params: {
+          lat,
+          lon,
+          appid: API_KEY,
+          units: 'metric',
+        },
+      });
+      // console.log(response.data);
+      this.isDataLoaded = true;
+      this.apiData = response.data || {};
+      this.currentConditions = {
+        city: response.data.name,
+        country: response.data.sys.country,
+        temp: response.data.main.temp,
+        feelsLike: response.data.main.feels_like,
+        humidity: response.data.main.humidity,
+        description: response.data.weather[0].main,
+        icon: response.data.weather[0].icon,
+        windDir: response.data.wind.deg,
+        windSpeed: response.data.wind.speed, // meter/sec or miles/hour
+      } || {};
     },
   },
   computed: {
@@ -199,6 +229,10 @@ export default {
         ? `http://openweathermap.org/img/wn/${this.currentConditions.icon}@2x.png`
         : '';
     },
+
+    // setCountryFlag() {
+    //   return this.citiesData.forEach(city => city.country.toLowerCase())
+    // },
   },
 };
 </script>
