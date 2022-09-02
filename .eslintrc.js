e8a7f2bb61f64f37bed3354a6d47120c8f61b10e
Marina Gotovtseva <m.gotovtseva@labmedia.su>
2022-09-02 18:05:00 +0300

    Fixed the line max length

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': ['error', 'windows'],
-    'max-len': ['error', { code: 1000 }],
+    'max-len': ['error', { code: 250 }],
     'consistent-return': ['error', { treatUndefinedAsUnspecified: true }],
     'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
   },
