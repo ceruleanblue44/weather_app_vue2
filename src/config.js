bb125e7f4718c6daf05865271a2969a12432153f
Marina Gotovtseva <m.gotovtseva@labmedia.su>
2022-11-14 12:21:15 +0300

    trying to resolve the cors issue

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_KEY = 'd456af1653589db20bd812fbeb08a1f9';
-export const API_CURRENT_CONDITIONS = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_CURRENT_CONDITIONS = 'http://api.openweathermap.org/data/2.5/weather';
 export const API_GEOCODING = 'http://api.openweathermap.org/geo/1.0/direct';
 export const API_FLAGS = 'https://openweathermap.org/images/flags';
